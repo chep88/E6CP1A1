c0262f52d9b953e832d4b31f96e1719b7973d972
Jean Pierre Perez Varas <jperez@mesos.cl>
2017-06-13 19:52:07 -0400

    se muestran los divisores de 990 con todos los ciclos

--- 2 ciclos/ejercicio3.rb
+++ 2 ciclos/ejercicio3.rb
@@ -1,2 +1,26 @@
 # Mostrar todos los divisores del número 990 con:
 # while, for, times.
+i=1
+puts 'Divisores con While'
+puts '==================='
+while i<=990
+   puts i if (990 % i).zero?
+   i+=1
+end
+puts '==================='
+puts 'Divisores con For'
+puts '==================='
+(1..990).each do |x|
+  puts x if (990 % x).zero?
+end
+puts '==================='
+puts 'Divisores con Times'
+
+
+puts '===Inicio Times===='
+
+990.times do |z|
+  puts z if (990 % (z+1)).zero?
+end
+
+puts '===Fin Times======='
