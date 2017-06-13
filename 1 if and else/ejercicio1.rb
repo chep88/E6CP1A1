16435e154f2aad0c8eda28eb79d0ecb78b87899d
Jean Pierre Perez Varas <jperez@mesos.cl>
2017-06-13 17:40:07 -0400

    se modifica segun lo indicado en el ejercicio

--- 1 if and else/ejercicio1.rb
+++ 1 if and else/ejercicio1.rb
@@ -1,4 +1,6 @@
 # Hacer que se cumpla la condición modificando la línea 4.
 
-a = 'saludo'
-puts 'La condición es verdadera.' if a == 'saludo'
+a = 2
+if a == 2
+  puts 'La condición es verdadera.'
+end
