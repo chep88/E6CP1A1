ffe9bbbf18d23ed3b70e2e7928db1b793ba03618
Jean Pierre Perez Varas <jperez@mesos.cl>
2017-06-13 23:27:31 -0400

    creo que se termina la logica aun no se prueba

--- 3 ciclos anidados/ejercicio3.rb
+++ 3 ciclos anidados/ejercicio3.rb
@@ -2,3 +2,23 @@
 # la tabla de multiplicar del número ingresado. Debe repetir la operación hasta
 # que se ingrese un 0 (cero).
 # Ingrese un número (0 para salir): _
+def mostrarTabla(num)
+  (1..10).each do |x|
+    puts "#{x}  * #{num} = #{x*num}"
+  end
+end
+
+def menu
+  puts " Mostrar Tabla (Ingresa el numero de la tabla que quieres ver) "
+  puts "0 ) Salir"
+end
+
+opcioMenu=0
+
+while opcioMenu != 0
+  menu3
+  opcioMenu = gets.chomp
+  if opcioMenu < 0
+    mostrarTabla(opcioMenu)
+  end
+end
