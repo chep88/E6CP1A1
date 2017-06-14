07fbaba731b3c046f7c87ea4f6d6c4d6ae6b938b
Jean Pierre Perez Varas <jperez@mesos.cl>
2017-06-13 21:13:50 -0400

    se genera un metodo para mostrar las columnas multiplicando por el numero de filas

--- 3 ciclos anidados/ejercicio1.rb
+++ 3 ciclos anidados/ejercicio1.rb
@@ -5,3 +5,14 @@ Se pide imprimir la secuencia numérica, de la siguiente forma:
 3   6   9   12
 4   8   12   16
 =end
+def pintaColumnas(multiplo = 1)
+  col = ''
+  (1..4).each do |i|
+    col =  col << "  #{i*multiplo}"
+  end
+  return col
+end
+
+(1..4).each do |x|
+  puts pintaColumnas(x)
+end
