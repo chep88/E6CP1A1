fec1d2f89ee710ca8125a5980f76968001474661
Jean Pierre Perez Varas <jperez@mesos.cl>
2017-06-13 20:31:31 -0400

    se recorre hasta 11 y se valida si el el inicio es 0, luego se valida si es par y se concatena i con la palabra par o impar si corresponde

--- 2 ciclos/ejercicio8.rb
+++ 2 ciclos/ejercicio8.rb
@@ -2,8 +2,14 @@
 # a = '1impar 2par 3impar 4par 5impar 6par 7impar 8par 9impar 10par'
 
 a = ''
-10.times do |i|
-	
+11.times do |i|
+	unless i.zero?
+		if i.even?
+			a= a << "#{i}par "
+		else
+			a= a << "#{i}impar "
+		end
+	end
 end
 
 puts a
